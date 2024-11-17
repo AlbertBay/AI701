89707c4daada119fdfb20cf8fd1885eaaa4c1994
Albert Baychorov <albert.baichorov@mbzuai.ac.ae>
2024-11-17 21:40:45 +0000

    pre final

---
+++ data/get_data_splits.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from utils import decompile
+import re as re
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_parquet('data/opcodes.parquet')
+
+df['malicious'] = df['malicious'].astype(int)
+df.drop(columns=['creation_bytecode'], inplace=True)
+df = df[df.opcode.duplicated() == False]
+
+print('Class balance', Counter(df.malicious))
+
+X = df[['opcode']]
+y = df.malicious
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.40, random_state=42, stratify= y )
+X_test, X_val, y_test, y_val = train_test_split( X_test, y_test, test_size=0.30, random_state=42, stratify= y_test )
+
+print(f'Test - {Counter(y_test)}')
+print(f'Val - {Counter(y_val)}')
+print(f'Train - {Counter(y_train)}')
+
+
+X_train['malicious'] = y_train
+X_val['malicious'] = y_val
+X_test['malicious'] = y_test
+
+X_train.reset_index(drop=True).to_parquet('data/train.parquet')
+X_val.reset_index(drop=True).to_parquet('data/val.parquet')
+X_test.reset_index(drop=True).to_parquet('data/test.parquet')
---
+++ utils.py
@@ -0,0 +1,23 @@
+from evmdasm import EvmBytecode
+import json
+import re
+
+def decompile(_bytecode: str):
+    disassembler = EvmBytecode(_bytecode)
+    opcode = disassembler.disassemble().as_string
+    opcode_normalized = opcode.replace(' \n', '\n').replace(' ', ' 0x')
+    return opcode_normalized.lower()
+
+
+def tokenize_with_hex_replacement(opcode_str):
+    modified_str = re.sub(r'0x[0-9a-fA-F]+', 'HEX_CONST', opcode_str)
+    tokens = re.split(r'\s+', modified_str)
+    return tokens
+
+
+def get_average_vector(opcode_list, model):
+    vectors = [model.wv[word] for word in opcode_list if word in model.wv]
+    if vectors:
+        return sum(vectors) / len(vectors)
+    else:
+        return [0] * model.vector_size
